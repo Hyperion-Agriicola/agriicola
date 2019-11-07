392fff4a0f5dfb22de0647bbd17008b5464f212e
Hyperion-Agriicola <54855331+Hyperion-Agriicola@users.noreply.github.com>
2019-11-07 14:20:28 -0600

    Querys
    Querys de Eliminacion bien Leches jujui

---
+++ Querys de eliminar LECHES/Enfermedades/delete_enfermedad.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eliminar</title>
+    
+</head>
+<body>
+    
+    <?php
+
+$id_agroquimico_recibido=$_REQUEST['id_agroquimico'];
+
+        require_once "db_config.php";
+        //establecer los datos para la conexion a la base de datos.
+        $conexion = new mysqli($servidor_db, $usuario_db, $pass_db, $nombre_db);
+
+        //capturar error
+        if ($conexion->connect_error){
+            echo "Ha ocurrido un error: ". $conexion->connect_errno;
+        }
+        //guardar las variables en la base de datos
+
+        $sql="UPDATE agroquimicos SET estatus='eliminado' WHERE id_agroquimico=".$id_agroquimico_recibido;
+        $resultado= $conexion->query($sql) ;
+        echo "Enfermedad eliminada   ";
+
+    ?> 
+    <a href="consultar_enfermedad.php">Ir al listado</a>
+
+
+</body>
+</html>
