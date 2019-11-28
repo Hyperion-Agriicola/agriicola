b9ee5bf10456c69bb33bcd97a5278448227d6858
Hyperion-Agriicola <hyperionagriicola@gmail.com>
2019-11-28 00:14:12 -0600

    Noticias

--- js/main.js
+++ js/main.js
@@ -277,14 +277,9 @@ function passwordReset() {
 }
 
 $(document).ready(function(){
-    $('#myCarouselCustom').carousel();
-
-// Go to the previous item
-$("#prevBtn").click(function(){
-    $("#myCarouselCustom").carousel("prev");
-});
-// Go to the previous item
-$("#nextBtn").click(function(){
-    $("#myCarouselCustom").carousel("next");
-});
+    $('.carousel').carousel({
+        interval: 2000
+      })
   });
+
+
--- views/user/noticias.php
+++ views/user/noticias.php
@@ -18,7 +18,12 @@
 <div class="container">
     <div class="row pt-5 mt-4">
     <div class="col-lg-12 col-xl-12">
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="../../img/imagen1.png" class="d-block w-100" alt="...">
@@ -30,11 +35,11 @@
       <img src="../../img/menu/imagen3.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
